8d35875cc46d6940cb63e2704acf7cabca1f4848
fzotes <fzotesf@alumnos.nebrija.es>
2022-01-02 18:37:03 +0000

    Commit fm2

--- maxflujo.py
+++ maxflujo.py
@@ -8,17 +8,7 @@ import matplotlib.pyplot as plt
 #from dwave.system.samplers import DWaveSampler
 #from dwave.system.composites import EmbeddingComposite
 
-#Crear un grafo de 4 nodos
-#G=nx.Graph()
-#G.add_nodes_from(["s","1","2","t"])
-#G.add_weighted_edges_from({("s","1", 1), ("s", "2", 2), ("1", "t", 2), ("2", "t", 1)})
-#G.add_edge("s","1")
-#G.add_edge("s","2")
-#G.add_edge("1","t")
-#G.add_edge("2","t")
-
 G=nx.Graph()
-#i=1
 G.add_node("s",pos=(1,1))
 G.add_node("1",pos=(2,2))
 G.add_node("2",pos=(2,0))
@@ -31,5 +21,19 @@ pos=nx.get_node_attributes(G,'pos')
 nx.draw(G,pos, with_labels=True)
 labels = nx.get_edge_attributes(G,'weight')
 nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-#nx.draw(G, with_labels = True, with_weights = True)
 plt.savefig("maxflujo.png")
+
+import dimod
+
+# Cálculo del flujo máximo
+# mediante un algoritmo clásico (Edmonds-Karp)
+#A maximum cut is a subset S of the vertices of G such that the number of edges between S and the complementary subset
+# is as large as possible.
+sampler = dimod.SimulatedAnnealingSampler()
+cut=dnx.weighted_maximum_cut(G,sampler)
+print(cut)
+
+# Matriz Q de restricciones QUBO
+alpha_s=0.3
+bqm = dimod.BinaryQuadraticModel({},{'aa': 1-alpha_s, 'ad': -2, 'ae': -2, 'bb': 1-alpha_s, 'bc': 2, 
+                           'bf': -2, 'cc': 1-alpha_s, 'cf': -2, 'dd': 1, 'de': 2, 'ee': 1, 'ff': 1},0,'BINARY')
